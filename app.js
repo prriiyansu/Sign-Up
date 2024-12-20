34d94a38d033044202fcfe294cfb77ff95ab5117
Priyansu <prriiyansunegi@gmail.com>
2024-12-21 05:16:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
+  // Redirect to the specified URL
+  window.location.href = "https://prriiyansunegi.github.io/Room-Dekho/";
 });
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
+  // Redirect to the specified URL
+  window.location.href = "https://prriiyansunegi.github.io/Room-Dekho/";
 });
